fc6e9dbfe20c1df0356f592c034a2f6769737970
Haicheng Wang <haicheng.w@opn.ooo>
2026-04-24 17:50:24 +0700

    AS-29 Wired the frontned with backend and defined render logic; Added back index.html.

--- helpscout-frontend/src/App.jsx
+++ helpscout-frontend/src/App.jsx
@@ -1,9 +1,78 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 import Sidebar from './Sidebar'
 import SidePanel from './SidePanel'
 
 function App() {
-  // define the routes
+  const [status, setStatus] = useState("loading"); // support "loading", "authenticated" and "error"
+
+  // comment out whole useEffect for local UI testing
+  useEffect(() => {
+    // get parameter set
+    const rawQuery = window.location.search;
+    if (!rawQuery) {
+      setStatus("error");
+      return; // short circuit
+    }
+
+    // verifiy signature
+    const verifySignature = async () => {
+      try {
+        const backendUrl = import.meta.env.VITE_API_BASE_URL;
+
+        const response = await fetch(`${backendUrl}/session/bootstrap`, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(
+            {raw_query: rawQuery}
+          )
+        });
+
+        if (response.ok) {
+          setStatus("authenticated");
+        } else {
+          console.error("verification failed: ", response.body, ", with status code: ", response.status);
+          setStatus("error");
+        }
+      } catch (error) {
+        console.error("Error during verification: ", error);
+        setStatus("error");
+      }
+    };
+
+    verifySignature();
+
+  }, []); // empty [] means run once when app starts
+
+  // render logic
+  if (status === 'loading') {
+    return (
+      <div style={{
+        padding: '20px',
+        fontFamily: 'system-ui, sans-serif',
+        color: '#666'
+        }}>
+          Verifying Helpscout signature...
+        </div>
+    );
+  }
+  
+  if (status === 'error') {
+    return (
+      <div style={{ 
+        padding: '20px', 
+        fontFamily: 'system-ui, sans-serif', 
+        color: '#c93030', 
+        fontWeight: 'bold' 
+        }}>
+        Unauthorized Access or Backend Unreachable.
+      </div>
+    );
+  }
+
+  // define the routes on authenticated status
   return (
     <HashRouter>
       <Routes>
